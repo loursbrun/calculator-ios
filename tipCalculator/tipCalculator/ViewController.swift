77d0dbe2cc8bc8104ed3e4985d788ba6cde1ed33
fabien.brun <fabienbrun@hotmail.fr>
2017-07-05 19:39:03 +0200

    Add calcul

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pourcentagePourboire:Int!
+    
     @IBOutlet weak var montantFactureTexfield: UITextField!
     @IBOutlet weak var pourboireLabel: UILabel!
     @IBOutlet weak var montantTotalLabel: UILabel!
@@ -18,23 +21,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        pourboireSegmentedControl.addTarget(self, action: #selector(ViewController.calculerPourBoire), for: UIControlEvents.valueChanged)
+        pourboireSegmentedControl.addTarget(self, action: #selector(ViewController.calculerPourcentage), for: UIControlEvents.valueChanged)
         
     }
 
-    func calculerPourBoire() {
+    func calculerPourcentage() {
     
         switch pourboireSegmentedControl.selectedSegmentIndex {
         case 0:
             print("segment selectionné:\(pourboireSegmentedControl.selectedSegmentIndex)")
+            pourcentagePourboire = 15
         case 1:
             print("segment selectionné:\(pourboireSegmentedControl.selectedSegmentIndex)")
+            pourcentagePourboire = 20
         case 2:
             print("segment selectionné:\(pourboireSegmentedControl.selectedSegmentIndex)")
+            pourcentagePourboire = 25
         default:
             print("Pas de selection")
         }
         
+        calculerPourboire(pourcent: Float(pourcentagePourboire!))
+        
+    }
+    
+    func calculerPourboire(pourcent:Float){
+        
+        let pourboire = Float(montantFactureTexfield.text!)! * pourcent / 100
+        
+        let montant = Float(montantFactureTexfield.text!)
+        
+        let pourBoireFormatStr = NSString(format: "%.2f", pourboire)
+        
+        let montantTotalFormatStr = NSString(format: "%.2f", pourboire + montant!)
+        
+        
+        
+        pourboireLabel.text = "€ \(pourBoireFormatStr)"
+        montantTotalLabel.text = "€\(montantTotalFormatStr)"
     }
 
 }
